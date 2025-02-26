cae2854f2c39d635587a41202f822f595f1778e1
liufanglue <liufanglue@126.com>
2025-02-27 00:48:20 +0800

    output total distance trend

--- oriKeans.py
+++ oriKeans.py
@@ -8,17 +8,19 @@ isNeedDebug = False
 
 def kmeans(X, k, max_iters = 100000):
     centroids = X[torch.randperm(X.shape[0])[ : k]]
-    for _ in range(max_iters):
+    for index in range(max_iters):
         distances = torch.cdist(X, centroids)
         labels = torch.argmin(distances, dim = 1)
         new_centroids = torch.stack([X[labels == i].mean(dim = 0) for i in range(k)])
         if torch.all(centroids == new_centroids):
             break
         centroids = new_centroids
-    
+
+        totalDistance = CalKmeansTotalDistance(X, labels, centroids)
+        print(f"kmeans index = {index}, totalDistance = {totalDistance}")
+
     PrintTorch(labels, 'labels', True)
     PrintTorch(centroids, 'centroids', True)
-    
     return labels, centroids
 
 def PrintTorch(data, des = '', isNeedPrintData = False):
@@ -34,30 +36,40 @@ def PrintTorch(data, des = '', isNeedPrintData = False):
             else:
                 print(f"des = {des}, shape = {data.shape}")
 
-n_points = 1000
-X = torch.randn(n_points, 3)
+def CalKmeansTotalDistance(X, labels, centroids):
+    sampleCenters = centroids[labels]
+    # 计算每个样本到其对应聚类中心的 1 范数
+    distances = torch.norm(X - sampleCenters, p = 1, dim = 1)
+    totalDistance = torch.sum(distances)
+    return totalDistance
 
-k = 2
+if __name__ == "__main__":
 
-startTime = datetime.now()
-labels, centroids = kmeans(X, k)
-endTime = datetime.now()
-print(f"time = {endTime - startTime}")
+    torch.manual_seed(10)
+    n_points = 1000
+    X = torch.randn(n_points, 3)
 
-fig = plt.figure(figsize = (10, 8))
-ax = fig.add_subplot(111, projection = '3d')
+    k = 2
 
-colors = ['r', 'g', 'b']
-for i in range(k):
-    cluster_points = X[labels == i]
-    ax.scatter(cluster_points[ : , 0], cluster_points[ : , 1], cluster_points[ : , 2], c = colors[i], label = f'Cluster {i+1}')
+    startTime = datetime.now()
+    labels, centroids = kmeans(X, k)
+    endTime = datetime.now()
+    print(f"time = {endTime - startTime}")
 
-ax.scatter(centroids[ : , 0], centroids[ : , 1], centroids[ : , 2], c = 'black', s = 200, marker = '*', label = 'Centroids')
+    fig = plt.figure(figsize = (10, 8))
+    ax = fig.add_subplot(111, projection = '3d')
 
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-ax.legend()
-plt.title('3D K-means Clustering')
-plt.show()
+    colors = ['r', 'g', 'b']
+    for i in range(k):
+        cluster_points = X[labels == i]
+        ax.scatter(cluster_points[ : , 0], cluster_points[ : , 1], cluster_points[ : , 2], c = colors[i], label = f'Cluster {i+1}')
+
+    ax.scatter(centroids[ : , 0], centroids[ : , 1], centroids[ : , 2], c = 'black', s = 200, marker = '*', label = 'Centroids')
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    ax.legend()
+    plt.title('3D K-means Clustering')
+    plt.show()
 
